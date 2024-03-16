77d2f26e3c8806b09b7c6f44b30701ae3dcfbca1
IgorAugust0 <augustoigor269@gmail.com>
2024-03-16 11:20:51 -0300

    add retrieve JWT (sem validação)
    - Add SecurityFilter: recupera bearer token auth gerado em auth company e imprime no console (p/ teste), ainda com 403 pois validacao ainda n implementada

--- src/main/java/com/igor/gestao_vagas/modules/security/SecurityConfig.java
+++ src/main/java/com/igor/gestao_vagas/modules/security/SecurityConfig.java
@@ -6,9 +6,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final SecurityFilter securityFilter;
+
+    public SecurityConfig(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
 
     @Bean // override do metodo original e desabilitando o csrf
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,7 +26,8 @@ public class SecurityConfig {
                             .requestMatchers("/company/").permitAll()
                             .requestMatchers("/auth/company").permitAll();
                     auth.anyRequest().authenticated();
-                });
+                })
+                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
         return http.build();
     }
 
